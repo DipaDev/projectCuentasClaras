0870882e8ffae72d75ffa6f50ccba965515f86ad
ddipaola <ddipaola@mailexternos.com.ar>
2016-05-18 16:53:42 -0300

    Agregado de validaciones Nuevo mensaje de bienvenida

--- func.py
+++ func.py
@@ -1,23 +1,52 @@
+# -*- coding: utf-8 -*-
+
 #Muestra un mensaje de bienvenida para el usuario
 def mensajeBienvenida():
-	print("""\
-		++++++++++++ Bienvenido a CUENTAS CLARAS 1.0 ++++++++++++ 
-				**Coded By DDP**
-		""")
+	print("\n")
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	print("+ --------------- CUENTAS CLARAS v1.0 ----------------  +")
+	print("+            Basta de papel y calculadora!              +")
+	print("+ **Coded By DDP**                                      +")
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	print("\n")
 
 #Pide al usuario que ingrese la cantidad de sueldos que van a cubrir los gastos
 def getCantidadAportes():
-	cantidadAportes = int(input('Por Favor Ingrese la cantidad de personas que van a aportar a los gastos (Ej: 1, 2, 4), luego de ingresar el numero presione Enter: '))
+	cantidadAportes = ""
+
+	while True:
+		cantidadAportes = int(input('Por Favor Ingrese la cantidad de personas que van a aportar a los gastos (Ej: 1, 2, 4), luego de ingresar el numero presione Enter: '))
+		print("La cantidad ingresada es = " + str(cantidadAportes))
+		conf = raw_input("¿Es correcta? (\"S\" = Si, \"N\" = No) ------>  ") #Para Python 2
+		#conf = input("¿Es correcta? (\"S\" = Si, \"N\" = No) ------>  ") #Para Python 3
+
+		if conf.lower() in ['s', 'S']:
+			break
+
 	return cantidadAportes
 
 #Pide al usuario que ingrese el monto neto de cada sueldo
 def getSueldosDePantalla(cantidadDeSueldos):
-	sueldos = []
-	i = 1
-	while i <= cantidadDeSueldos:
-		sueldoAIngresar = float(input('Ingrese el importe neto del ' +str(i)+' sueldo: '))
-		sueldos.append(sueldoAIngresar)
-		i += 1
+	
+
+	while True:
+		sueldos = []
+		i = 1
+		while i <= cantidadDeSueldos:
+			sueldoAIngresar = float(input('Ingrese el importe neto del ' +str(i)+' sueldo: '))
+			sueldos.append(sueldoAIngresar)
+			i += 1
+
+		print("Los gastos ingresados son:)
+		g = 1
+		while g <= sueldos:
+			print("Sueldo "+ str(g)+ " : "+str(sueldos[g-1])
+		
+		conf = raw_input("¿Los sueldos son correctos? (\"S\" = Si, \"N\" = No) ------>  ") #Para Python 2
+		#conf = input("¿Es correcta? (\"S\" = Si, \"N\" = No) ------>  ") #Para Python 3
+
+		if conf.lower() in ['s', 'S']:
+			break		
 
 	return sueldos
 
---
+++ prueba.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+while True:
+	conf = raw_input("¿Es correcta? (\"S\" = Si, \"N\" = No) ------>  ")
+	print (conf)
